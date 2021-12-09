703d5c37e5d20887cd5686b272b396d732fc55f2
Mahima <mahima@Mahimas-MacBook-Air.local>
2021-12-08 22:30:15 -0500

    Added Address Details Class

---
+++ AED_Project/src/Business/Adopt/AddressDetails.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Adopt;
+
+/**
+ *
+ * @author raomah
+ */
+public class AddressDetails {
+    private String Street;
+    private String city;
+    private String State;
+
+    public String getStreet() {
+        return Street;
+    }
+
+    public void setStreet(String Street) {
+        this.Street = Street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+    
+    
+}
