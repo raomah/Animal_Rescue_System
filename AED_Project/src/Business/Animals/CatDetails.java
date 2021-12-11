fdf5b5bbb9205f88cba5b176536cc86fc1a05d4b
Dongreneha <dongre.n@northeastern.edu>
2021-12-11 05:52:01 -0500

    Added

---
+++ AED_Project/src/Business/Animals/CatDetails.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Animals;
+
+/**
+ *
+ * @author user
+ */
+public class CatDetails extends AnimalDetails{
+    private String catbreed;
+    private String catcolor;
+    private String catpersonality;
+
+    public String getCatbreed() {
+        return catbreed;
+    }
+
+    public void setCatbreed(String catbreed) {
+        this.catbreed = catbreed;
+    }
+
+    public String getAnimalcolor() {
+        return catcolor;
+    }
+
+    public void setAnimalcolor(String color) {
+        this.catcolor = color;
+    }
+
+    public String getCatpersonality() {
+        return catpersonality;
+    }
+
+    public void setCatpersonality(String catpersonality) {
+        this.catpersonality = catpersonality;
+    }
+    
+}
