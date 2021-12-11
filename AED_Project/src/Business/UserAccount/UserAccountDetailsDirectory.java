9445dabe3b6643ae0c225dcdb4e7a0447725188b
Krishna Dubey <krishna@Krishnas-MacBook-Air.local>
2021-12-11 06:30:33 -0500

    visitor details

--- AED_Project/src/Business/UserAccount/UserAccountDetailsDirectory.java
+++ AED_Project/src/Business/UserAccount/UserAccountDetailsDirectory.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 
 /**
  *
- * @author user
+ * @author krishna
  */
 public class UserAccountDetailsDirectory {
     private ArrayList<UserAccountDetails> userAccountList;
