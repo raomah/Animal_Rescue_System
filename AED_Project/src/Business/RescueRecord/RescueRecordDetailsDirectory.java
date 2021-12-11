ca4b72afaf68ff1ed6552013d43d44783d2b9866
raomah <rao.mah@northeastern.edu>
2021-12-11 07:32:22 -0500

    Files Added

---
+++ AED_Project/src/Business/RescueRecord/RescueRecordDetailsDirectory.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.RescueRecord;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author user
+ */
+public class RescueRecordDetailsDirectory {
+    private ArrayList<RescueRecordDetails> rescueRecordList;
+
+    public RescueRecordDetailsDirectory() {
+        rescueRecordList = new ArrayList<RescueRecordDetails>(); 
+    }
+
+    public ArrayList<RescueRecordDetails> getRescueRecordList() {
+        return rescueRecordList;
+    }
+
+    public void setRescueRecordList(ArrayList<RescueRecordDetails> rescueRecordList) {
+        this.rescueRecordList = rescueRecordList;
+    }
+    
+    public void addRescueRecord(RescueRecordDetails r) {
+        rescueRecordList.add(r);
+    }
+    
+    public void removeRescueRecord(RescueRecordDetails r) {
+        rescueRecordList.remove(r);
+    }
+    
+}
