6b491cc5b060facc4f1bb76b29d0f80db029612c
Krishna Dubey <krishna@Krishnas-MacBook-Air.local>
2021-12-11 06:56:37 -0500

    treatment

---
+++ AED_Project/src/Business/WorkQueue/TreatmentWre.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.WorkQueue;
+
+import Business.Animals.AnimalDetails;
+
+/**
+ *
+ * @author user
+ */
+public class TreatmentWre extends AllWorkRequest{
+    private AnimalDetails animal;
+}
