6882d5e4554700a3f0de1c120e849157bc8eef57
raomah <rao.mah@northeastern.edu>
2021-12-11 06:53:06 -0500

    Files Added

---
+++ AED_Project/src/Business/Organisation/PharmacyOrganisation.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Organisation;
+
+import Business.Role.PharmacistRole;
+import Business.Role.AllRoles;
+import Business.Medicines.MedicineDetailsDirectory;
+import Business.WorkQueue.AllWorkQueue;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author user
+ */
+public class PharmacyOrganisation extends OrganisationMain{
+    private MedicineDetailsDirectory medicineDirectory;
+    private AllWorkQueue workQueue;
+    
+    
+    public PharmacyOrganisation() {
+        super(OrganisationMain.Type.Pharmacy.getValue());
+    }
+
+    public MedicineDetailsDirectory getMedicineDirectory() {
+        return medicineDirectory;
+    }
+
+    public void setMedicineDirectory(MedicineDetailsDirectory medicineDirectory) {
+        this.medicineDirectory = medicineDirectory;
+    }
+    
+    @Override
+    public ArrayList<AllRoles> getSupportedRole() {
+        ArrayList<AllRoles> roles = new ArrayList<AllRoles>();
+        roles.add(new PharmacistRole());
+        return roles;
+    }
+
+    
+}
