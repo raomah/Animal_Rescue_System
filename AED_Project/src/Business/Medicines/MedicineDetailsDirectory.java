d23d993dc9e6b782a0e0ae637ca1e4cf4b5d4b85
raomah <rao.mah@northeastern.edu>
2021-12-11 06:22:13 -0500

    Files Added

---
+++ AED_Project/src/Business/Medicines/MedicineDetailsDirectory.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Medicines;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author user
+ */
+public class MedicineDetailsDirectory {
+    private ArrayList<MedicineDetails> medicineList;
+
+    public MedicineDetailsDirectory() {
+        medicineList = new ArrayList<MedicineDetails>(); 
+    }
+
+    public ArrayList<MedicineDetails> getMedicineList() {
+        return medicineList;
+    }
+
+    public void setMedicineList(ArrayList<MedicineDetails> medicineList) {
+        this.medicineList = medicineList;
+    }
+    
+    public void addMedicine(MedicineDetails m) {
+        medicineList.add(m);
+    }
+    
+    public void removeMedicine(MedicineDetails m) {
+        medicineList.remove(m);
+    }
+}
