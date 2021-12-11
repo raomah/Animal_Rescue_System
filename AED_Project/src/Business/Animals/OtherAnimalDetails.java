99a39e8623e39044cf235f7fd1a19888ffc395fe
Dongreneha <dongre.n@northeastern.edu>
2021-12-11 05:53:36 -0500

    Added

---
+++ AED_Project/src/Business/Animals/OtherAnimalDetails.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Animals;
+
+/**
+ *
+ * @author user
+ */
+public class OtherAnimalDetails extends AnimalDetails{
+    private String animaltype;
+    private String animalbreed;
+    private String animalcolor;
+    private String animalpersonality;
+
+    public String getOtherAnimalType() {
+        return animaltype;
+    }
+
+    public void setOtherAnimalType(String type) {
+        this.animaltype = type;
+    }
+
+    public String getAnimalbreed() {
+        return animalbreed;
+    }
+
+    public void setAnimalbreed(String animalbreed) {
+        this.animalbreed = animalbreed;
+    }
+
+    public String getAnimalcolor() {
+        return animalcolor;
+    }
+
+    public void setAnimalcolor(String color) {
+        this.animalcolor = color;
+    }
+
+    public String getAnimalpersonality() {
+        return animalpersonality;
+    }
+
+    public void setAnimalpersonality(String animalpersonality) {
+        this.animalpersonality = animalpersonality;
+    }
+}
